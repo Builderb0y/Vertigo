de2f1ad2813811c28bc9b2ade9e82ecac006c7e5
Builderb0y <14227067+Builderb0y@users.noreply.github.com>
2024-12-18 18:00:50 -0600

    don't cache packets which should be different per-player.

--- readme.md
+++ readme.md
@@ -1,15 +1,19 @@
-By default, minecraft keeps entire chunks synced with players. Which is problematic when using mods or data packs which make the world taller. Big Globe for example makes the overworld 2048 blocks tall. The reason why this is problematic is two-fold:
+By default, Minecraft keeps entire chunks synced with players, which is problematic when using mods or data packs which make the world taller. Big Globe for example makes the overworld 2048 blocks tall. The reason why this is problematic is two-fold:
 1. It means there's a lot more data to sync from the server to the client in multiplayer, which increases bandwidth requirements.
 2. It means there's more blocks the client needs to keep track of, which increases memory usage.
 
-I didn't find a mod that can solve these problems during my brief search on Modrinth, so I made my own.
+Minecraft started delaying chunk syncing in 1.21 to adapt to the player's internet connection, and while this does help a lot,
+* A lot of mods are still on MC 1.20.1, and
+* The problem is still not solved completely. The player is less likely to time out now, but chunk syncing is overall slower.
+
+I didn't find a mod that can solve all of the above problems during my brief search on Modrinth, so I made my own.
 
 Vertigo attempts to solve these issues by only syncing chunk sections that are near the player vertically. In other words, by lying to the client about what blocks are where. If a chunk section is too far above or below the player, the server will tell the client that the section is empty. Or in other words, full of air.
 
 # Mod compatibility
 
 Because the client does not know about all the blocks in a chunk, it is expected that other mods may try to sync data in sections the client doesn't know about, with undefined results. To help reduce some of these problems, Vertigo does the following:
-* Overwrite `PlayerLookup.tracking(ServerWorld, BlockPos)` to not include players which are out of vertical range of the input position. If your mod uses this method to send update packets to players, it will probably work just fine.
+* Overwrite `PlayerLookup.tracking(ServerWorld, BlockPos)` in Fabric's networking API to not include players which are out of vertical range of the input position. If your mod uses this method to send update packets to players, it will probably work just fine.
 	* `PlayerLookup.tracking(BlockEntity)` delegates to `tracking(ServerWorld, BlockPos)`, so this tracking method should work just fine too.
 	* All other tracking methods are unchanged.
 * Include an API which allows:
--- src/1.20.1/java/builderb0y/vertigo/mixin/ThreadedAnvilChunkStorage_TrackPlayer.java
+++ src/1.20.1/java/builderb0y/vertigo/mixin/ThreadedAnvilChunkStorage_TrackPlayer.java
@@ -1,17 +1,25 @@
 package builderb0y.vertigo.mixin;
 
+import java.util.BitSet;
+
+import com.llamalad7.mixinextras.sugar.Local;
 import org.apache.commons.lang3.mutable.MutableObject;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.minecraft.network.packet.s2c.play.ChunkDataS2CPacket;
 import net.minecraft.server.network.ServerPlayerEntity;
+import net.minecraft.server.world.ServerLightingProvider;
 import net.minecraft.server.world.ThreadedAnvilChunkStorage;
 import net.minecraft.util.math.ChunkPos;
 import net.minecraft.world.chunk.WorldChunk;
+import net.minecraft.world.chunk.light.LightingProvider;
 
 import builderb0y.vertigo.VerticalTrackingManager;
 import builderb0y.vertigo.Vertigo;
@@ -19,14 +27,38 @@ import builderb0y.vertigo.Vertigo;
 @Mixin(ThreadedAnvilChunkStorage.class)
 public class ThreadedAnvilChunkStorage_TrackPlayer {
 
-	@Inject(method = "sendChunkDataPackets", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/packet/s2c/play/ChunkDataS2CPacket;<init>(Lnet/minecraft/world/chunk/WorldChunk;Lnet/minecraft/world/chunk/light/LightingProvider;Ljava/util/BitSet;Ljava/util/BitSet;)V"))
-	private void vertigo_capturePlayer(ServerPlayerEntity player, MutableObject<ChunkDataS2CPacket> cachedDataPacket, WorldChunk chunk, CallbackInfo callback) {
-		Vertigo.SYNCING_PLAYER.set(player);
+	@Shadow @Final private ServerLightingProvider lightingProvider;
+
+	/**
+	by default, minecraft will cache the chunk packet
+	so that it can be sent to multiple players.
+	this is problematic for vertigo,
+	since the players could be at different Y levels,
+	and should therefore receive different packets.
+
+	the method below, {@link #vertigo_createNewPacket(MutableObject, ServerPlayerEntity, WorldChunk)},
+	is used to create a new packet for each player.
+	this method here is used to not create a packet
+	which will never get sent anywhere.
+	*/
+	@Redirect(method = "sendChunkDataPackets", at = @At(value = "NEW", target = "(Lnet/minecraft/world/chunk/WorldChunk;Lnet/minecraft/world/chunk/light/LightingProvider;Ljava/util/BitSet;Ljava/util/BitSet;)Lnet/minecraft/network/packet/s2c/play/ChunkDataS2CPacket;"))
+	private ChunkDataS2CPacket vertigo_dontCachePacket(WorldChunk chunk, LightingProvider lightProvider, BitSet skyBits, BitSet blockBits) {
+		return null;
 	}
 
-	@Inject(method = "sendChunkDataPackets", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/packet/s2c/play/ChunkDataS2CPacket;<init>(Lnet/minecraft/world/chunk/WorldChunk;Lnet/minecraft/world/chunk/light/LightingProvider;Ljava/util/BitSet;Ljava/util/BitSet;)V", shift = Shift.AFTER))
+	@Redirect(method = "sendChunkDataPackets", at = @At(value = "INVOKE", target = "Lorg/apache/commons/lang3/mutable/MutableObject;getValue()Ljava/lang/Object;", ordinal = 1))
+	private Object vertigo_createNewPacket(MutableObject<?> instance, @Local(argsOnly = true) ServerPlayerEntity player, @Local(argsOnly = true) WorldChunk chunk) {
+		Vertigo.SYNCING_PLAYER.set(player);
+		try {
+			return new ChunkDataS2CPacket(chunk, this.lightingProvider, null, null);
+		}
+		finally {
+			Vertigo.SYNCING_PLAYER.set(null);
+		}
+	}
+
+	@Inject(method = "sendChunkDataPackets", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayerEntity;sendChunkPacket(Lnet/minecraft/util/math/ChunkPos;Lnet/minecraft/network/packet/Packet;)V", shift = Shift.AFTER))
 	private void vertigo_uncapturePlayer(ServerPlayerEntity player, MutableObject<ChunkDataS2CPacket> cachedDataPacket, WorldChunk chunk, CallbackInfo callback) {
-		Vertigo.SYNCING_PLAYER.set(null);
 		VerticalTrackingManager manager = VerticalTrackingManager.PLAYERS.computeIfAbsent(player, VerticalTrackingManager::new);
 		manager.onChunkLoaded(player, chunk.getPos().x, chunk.getPos().z);
 	}
