71b7f6fde82bb524f08db91c67256a54caa9d9b6
Builderb0y <14227067+Builderb0y@users.noreply.github.com>
2025-09-22 19:07:37 -0500

    saner handling of other mods calling setBlockState() on the wrong thread, add java argument to detect and debug issues like these.

--- src/common/java/builderb0y/vertigo/mixin/WorldChunk_SyncSkylight.java
+++ src/common/java/builderb0y/vertigo/mixin/WorldChunk_SyncSkylight.java
@@ -1,7 +1,10 @@
 package builderb0y.vertigo.mixin;
 
+import java.util.ConcurrentModificationException;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -15,9 +18,13 @@ import net.minecraft.world.World;
 import net.minecraft.world.chunk.WorldChunk;
 
 import builderb0y.vertigo.TrackingManager;
+import builderb0y.vertigo.Vertigo;
 
 @Mixin(value = WorldChunk.class, priority = 500) //before scalable lux.
 public abstract class WorldChunk_SyncSkylight {
+
+	@Unique
+	private static final boolean VERTIGO_TRACE_THREADS = Boolean.getBoolean("vertigo.traceWrongThreadForSetBlockState");
 
 	@Shadow public abstract World getWorld();
 
@@ -33,10 +40,36 @@ public abstract class WorldChunk_SyncSkylight {
 		CallbackInfoReturnable<BlockState> callback
 	) {
 		if (this.getWorld() instanceof ServerWorld serverWorld) {
-			for (ServerPlayerEntity player : serverWorld.getPlayers()) {
-				TrackingManager manager = TrackingManager.PLAYERS.get(player);
-				if (manager != null) manager.onLightingChanged(pos);
+			if (serverWorld.getServer().isOnThread()) {
+				for (ServerPlayerEntity player : serverWorld.getPlayers()) {
+					TrackingManager manager = TrackingManager.PLAYERS.get(player);
+					if (manager != null) manager.onLightingChanged(pos);
+				}
 			}
+			else {
+				serverWorld.getServer().execute(() -> {
+					for (ServerPlayerEntity player : serverWorld.getPlayers()) {
+						TrackingManager manager = TrackingManager.PLAYERS.get(player);
+						if (manager != null) manager.onLightingChanged(pos);
+					}
+				});
+			}
+		}
+	}
+
+	@Inject(method = "setBlockState", at = @At("HEAD"))
+	private void vertigo_checkThread(
+		BlockPos pos,
+		BlockState state,
+		#if MC_VERSION >= MC_1_21_5
+			int flags,
+		#else
+			boolean moved,
+		#endif
+		CallbackInfoReturnable<BlockState> callback
+	) {
+		if (VERTIGO_TRACE_THREADS && this.getWorld() instanceof ServerWorld serverWorld && !serverWorld.getServer().isOnThread()) {
+			Vertigo.LOGGER.warn("", new ConcurrentModificationException("Caught another mod being naughty and calling setBlockState() from the wrong thread. See the stack trace below to find out who to blame."));
 		}
 	}
 }
