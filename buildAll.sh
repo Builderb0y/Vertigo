3baab166727eafe7f96a6c1ba0d5b8007c7b2477
Builderb0y <14227067+Builderb0y@users.noreply.github.com>
2026-04-17 19:18:28 -0500

    don't classload minecraft's classes too early.

--- buildAll.sh
+++ buildAll.sh
@@ -12,4 +12,5 @@ export JAVA_HOME=/home/builderb0y/java/jdk-21.0.4+7/;
 ./gradlew "Switch to 1.21.5" && ./gradlew build && \
 ./gradlew "Switch to 1.21.8" && ./gradlew build && \
 ./gradlew "Switch to 1.21.10" && ./gradlew build && \
+./gradlew "Switch to 1.21.11" && ./gradlew build && \
 ./gradlew --stop;
